956edc7d887eec2c8c2bec51f58b74c2bd5bab80
Bevingta <123000377+Bevingta@users.noreply.github.com>
2025-05-10 13:13:18 -0400

    gpt streamlined

--- ChatGPT/run_chatgpt.py
+++ ChatGPT/run_chatgpt.py
@@ -27,7 +27,7 @@ def get_cvss_score_from_func(func_code):
         return "0.01"
 
 
-path = "C:/Users/saram/OneDrive/Desktop/primevul_for_gpt.json"
+path = "primevul_for_gpt.json"
 
 with open(path, 'r', encoding='utf-8') as file:
     data = json.load(file)
