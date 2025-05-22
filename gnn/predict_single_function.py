67dda257ee802b47f9905f76cf391a371fd451f6
alexv26 <velsmida@bc.edu>
2025-05-21 20:59:22 -0400

    Updated single-function prediction code (gnn)

--- gnn/predict_single_function.py
+++ gnn/predict_single_function.py
@@ -5,6 +5,7 @@ from data.GraphDataset import GraphDataset
 from data.graph_gen.graph_generator import generate_one_graph
 from gensim.models import Word2Vec
 from torch_geometric.loader import DataLoader
+import re
 def predict_single_function(func_entry, model_path, w2v_model_path, model_type="rgcn", hidden_dim=128, output_dim=2, dropout=0.5, device=None, threshold=0.5):
     if device is None:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -41,14 +42,90 @@ def predict_single_function(func_entry, model_path, w2v_model_path, model_type="
     return "Vulnerable" if is_vulnerable else "Safe"
 
 
+def extract_all_c_functions(file_path):
+    with open(file_path, 'r') as f:
+        code = f.read()
+
+    # Strip comments (basic but sufficient here)
+    import re
+    code = re.sub(r'/\*.*?\*/', '', code, flags=re.DOTALL)
+    code = re.sub(r'//.*', '', code)
+
+    functions = []
+    i = 0
+    while i < len(code):
+        # Skip until we find a likely function header (ends with ') {')
+        header_match = re.search(r'[a-zA-Z_][\w\s\*\(\),\[\]]+\)\s*\{', code[i:])
+        if not header_match:
+            break
+
+        start = i + header_match.start()
+        i = start
+        brace_count = 0
+        in_string = False
+        escape = False
+        func_end = None
+
+        while i < len(code):
+            c = code[i]
+
+            if c == '"' and not escape:
+                in_string = not in_string
+            elif c == '\\' and not escape:
+                escape = True
+                i += 1
+                continue
+            else:
+                escape = False
+
+            if in_string:
+                i += 1
+                continue
+
+            if c == '{':
+                brace_count += 1
+            elif c == '}':
+                brace_count -= 1
+                if brace_count == 0:
+                    func_end = i + 1
+                    break
+            i += 1
+
+        if func_end:
+            func_code = code[start:func_end].strip()
+            functions.append(func_code)
+            i = func_end
+        else:
+            # failed to close properly
+            break
+
+    return functions
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Test a function for vulnerabilities through a pre-trained model")
-    parser.add_argument("--func", type=str, required=True)
+    parser.add_argument("--func", type=str, default=None, required=False)
+    parser.add_argument("--func-path", type=str, default=None, required=False)
     parser.add_argument("--saved-model-path", type=str, default="saved_models/saved_model.pth")
     parser.add_argument("--w2v", type=str, default="data/w2v/word2vec_code.model")
     args = parser.parse_args()
 
-    func, model_path, w2v = args.func, args.saved_model_path, args.w2v
+    func, func_path, model_path, w2v = args.func, args.func_path, args.saved_model_path, args.w2v
 
-    prediction = predict_single_function(func, model_path, w2v)
-    print(f"This code is predicted to be {prediction}")
+    if func and func_path:
+        raise ValueError("Only one of 'func' or 'func_path' should be provided, not both.")
+
+    if (func and func_path) or (not func and not func_path):
+        raise ValueError("You must provide exactly one of 'func' or 'func_path'.")
+    
+    if func_path:
+        funcs = extract_all_c_functions(func_path)
+    if func:
+        funcs = [func]
+    
+    print(len(funcs))
+
+    for func in funcs:
+        prediction = predict_single_function(func, model_path, w2v)
+        print(func)
+        print(f"This code in the above function is predicted to be {prediction}")
